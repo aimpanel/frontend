71f93da36eff17b49e7888bc02a7da388963bffd
systemz <systemz@pc>
2015-09-10 17:17:27 +0200

    Add new server MC function

--- src/views/services/mc/list/index.js
+++ src/views/services/mc/list/index.js
@@ -12,7 +12,7 @@ module.exports = {
         this.serverSlug = this.route.params.serverSlug;
 
         //this.$root.checkPerm('HOSTS_MC_LIST');
-	//this.$root.checkPerm('');
+        //this.$root.checkPerm('');
 
         this.$root.$set('title', 'Lista serwerów MC');
         jQuery('.button-collapse').sideNav('hide');
@@ -23,19 +23,28 @@ module.exports = {
     methods: {
         getServers: function () {
             var self = this;
-            //get all hosts
-            this.$http.get(window.baseurl+'/api/v1/services/mc', function (data,status) {
-              console.log(status);
-                if (status === 200) {
-                   self.$set("servers", data);
-                }
+            this.$http.get(window.baseurl + '/api/v1/services/mc', function (data, status) {
+                self.$set("servers", data);
             });
         },
-        addServerModal:function(){
+        addServerModal: function () {
             jQuery('#addServer').openModal();
+        },
+        addServer: function () {
+            this.$http.post(window.baseurl + '/api/v1/services/mc', {"id": jQuery('#newServerId').val()},
+            function (data, status) {
+                this.getServers();
+                jQuery('#addServer').closeModal();
+                Materialize.toast('Serwer dodano', 2500);
+            }).error(function (data)
+            {
+                jQuery('#newServerId').removeClass('valid');
+                jQuery('#newServerId').addClass('invalid');
+                jQuery('#newServerValidator').attr("data-error", data.error_msg);
+            });
         }
     },
-    beforeDestroy: function() {
-        clearInterval(window.interval)
+    beforeDestroy: function () {
+        clearInterval(window.interval);
     }
 }
