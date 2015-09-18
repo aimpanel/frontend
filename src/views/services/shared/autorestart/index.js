d28941109fef85ba170ef453b7c978472def052d
Michał Frąckiewicz <michal@lvlup.pro>
2015-09-18 16:51:29 +0200

    Autorestart page for mc and ts3

---
+++ src/views/services/shared/autorestart/index.js
@@ -0,0 +1,24 @@
+require('./style.css');
+module.exports = {
+    template: require('./template.html'),
+    replace: true,
+    data: function () {
+        return {
+            id: false,
+            service: false,
+            servers: []
+        }
+    },
+    ready: function () {
+        this.$root.$set('title', 'Autorestart');
+        this.service = this.route.params.service;
+        this.id = this.route.params.id;
+        jQuery('.tooltipped').tooltip({delay: 50});
+        jQuery(".dropdown-button").dropdown({
+            hover: false,
+            belowOrigin: true,
+            constrain_width: true,
+            alignment: 'right'
+        });
+    }
+}
