d3c3d13708b1ea7f3597f31fc2a0aa77f763242f
Michał Frąckiewicz <michal@lvlup.pro>
2015-10-19 11:53:05 +0200

    Icons in sidebar, translated servers tab

--- src/locales/en.js
+++ src/locales/en.js
@@ -1,44 +1,46 @@
 window.locale.en = {
     general: {
-        notfound: 'Error 404 - Page not found',
-        notfoundNote: 'Instead have a cat gif',
-        close: 'Close',
-        loading: 'Loading'
+        notfound: "Error 404 - Page not found",
+        notfoundNote: "Instead have a cat gif",
+        close: "Close",
+        loading: "Loading"
     },
     home: {
-        'news': 'News'
+        "news": "News"
     },
     auth: {
-        'enterLoginAndPassword': 'Enter username and password',
-        'signIn': 'Sign in',
-        'clickToSignIn': 'Sign in',
-        'logout': 'Logout',
-        'signInToExistingAccount': 'Login to existing account',
-        'yourUsername': 'Your username',
-        'yourPassword': 'Your password',
-        'loggingIn': ' Logging in...'
+        "enterLoginAndPassword": "Enter username and password",
+        "signIn": "Sign in",
+        "clickToSignIn": "Sign in",
+        "logout": "Logout",
+        "signInToExistingAccount": "Login to existing account",
+        "yourUsername": "Your username",
+        "yourPassword": "Your password",
+        "loggingIn": " Logging in..."
     },
     about: {
         panel: "About panel",
-        thanks: 'Aimpanel exists thanks to wonderful projects, almost all of them are open source.',
-        specialThanks: 'Special thanks for:',
-        license: 'License',
-        proprietaryLicense: 'Proprietary license'
+        thanks: "Aimpanel exists thanks to wonderful projects, almost all of them are open source.",
+        specialThanks: "Special thanks for:",
+        license: "License",
+        proprietaryLicense: "Proprietary license"
     },
     security: {
-        unauthorized: 'You are not authorized to display this page'
+        unauthorized: "You are not authorized to display this page"
     },
     license: {
-        active: 'Active',
-        yes: 'Yes',
-        no: 'No',
-        forUser: 'For',
+        active: "Active",
+        yes: "Yes",
+        no: "No",
+        forUser: "For",
         desc: "Description",
         validTo: "Valid to",
         thanks: "Thank you for choosing Aimpanel :)"
     },
     servers: {
-        state: 'State',
+        servers: "Servers",
+        listOfServers: "List of servers",
+        state: "State",
         console: "Console",
         actions: "Actions",
         remove: "Remove",
@@ -48,10 +50,12 @@ window.locale.en = {
         emptyMaybeAddNew: "No servers. Maybe it's worth to try add a new? :)",
         addTooltip: "Protip: in lower right corner there is red plus sign",
         addServer: "Add server",
+        serverAdded: "Server added",
         addModal: {
             idLabel: "Server ID e.g. 1",
             clickToAdd: "Add",
-            working: "Working..."
+            working: "Working...",
+            serverType: "Type of server"
         },
         removeModal: {
             removeServer: "Delete server",
--- src/locales/pl.js
+++ src/locales/pl.js
@@ -1,44 +1,46 @@
 window.locale.pl = {
     general: {
-        notfound: 'Błąd 404 - Nie znaleziono strony',
-        notfoundNote: 'Dajemy Ci za to gifa z kotem',
-        close: 'Zamknij',
-        loading: 'Ładowanie'
+        notfound: "Błąd 404 - Nie znaleziono strony",
+        notfoundNote: "Dajemy Ci za to gifa z kotem",
+        close: "Zamknij",
+        loading: "Ładowanie"
     },
     home: {
-        'news': 'Aktualności'
+        "news": "Aktualności"
     },
     auth: {
-        'enterLoginAndPassword': 'Wpisz login i hasło',
-        'signIn': 'Logowanie',
-        'clickToSignIn': 'Zaloguj się',
-        'logout': 'Wyloguj się',
-        'signInToExistingAccount': 'Zaloguj się do istniejącego konta',
-        'yourUsername': 'Twoja nazwa użytkownika',
-        'yourPassword': 'Twoje hasło',
-        'loggingIn': ' Logowanie...'
+        "enterLoginAndPassword": "Wpisz login i hasło",
+        "signIn": "Logowanie",
+        "clickToSignIn": "Zaloguj się",
+        "logout": "Wyloguj się",
+        "signInToExistingAccount": "Zaloguj się do istniejącego konta",
+        "yourUsername": "Twoja nazwa użytkownika",
+        "yourPassword": "Twoje hasło",
+        "loggingIn": " Logowanie..."
     },
     about: {
         panel: "O panelu",
-        thanks: 'Aimpanel powstał dzięki wspaniałym projetom osób trzecich, większość jest otwartoźródłowa.',
-        specialThanks: 'Szczególne podziękowania dla:',
-        license: 'Licencja',
-        proprietaryLicense: 'licencja własnościowa'
+        thanks: "Aimpanel powstał dzięki wspaniałym projetom osób trzecich, większość jest otwartoźródłowa.",
+        specialThanks: "Szczególne podziękowania dla:",
+        license: "Licencja",
+        proprietaryLicense: "licencja własnościowa"
     },
     security: {
-        unauthorized: 'Brak uprawnień do wyświetlenia tej strony'
+        unauthorized: "Brak uprawnień do wyświetlenia tej strony"
     },
     license: {
-        active: 'Aktywna',
-        yes: 'Tak',
-        no: 'Nie',
-        forUser: 'Dla',
+        active: "Aktywna",
+        yes: "Tak",
+        no: "Nie",
+        forUser: "Dla",
         desc: "Opis",
         validTo: "Ważna do",
         thanks: "Dziękujemy za używanie Aimpanel :)"
     },
     servers: {
-        state: 'Stan',
+        servers: "Servers",
+        listOfServers: "Lista serwerów",
+        state: "Stan",
         console: "Konsola",
         actions: "Akcje",
         remove: "Usuń",
@@ -48,10 +50,12 @@ window.locale.pl = {
         emptyMaybeAddNew: "Brak serwerów. Może warto dodać nowy? :)",
         addTooltip: "Protip: w prawym dolnym rogu znajduje się plusik",
         addServer: "Dodaj serwer",
+        serverAdded: "Serwer dodano",
         addModal: {
             idLabel: "Identyfikator serwera np. 1",
             clickToAdd: "Dodaj",
-            working: "Pracuję..."
+            working: "Pracuję...",
+            serverType: "Typ serwera"
         },
         removeModal: {
             removeServer: "Usuń serwer",
