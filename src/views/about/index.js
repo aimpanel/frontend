be841d79939418ccead592eb677decebc35da2e7
Michał Frąckiewicz <michal@lvlup.pro>
2015-10-22 17:20:22 +0200

    List of all composer packs in about

--- src/views/about/index.js
+++ src/views/about/index.js
@@ -1,11 +1,25 @@
 require('./style.css');
 module.exports = {
     template: require('./template.html'),
+    data: function () {
+        return {
+            isLoading: true,
+            licenses: false
+        }
+    },
     replace: true,
     ready: function () {
         this.$root.$set('title', this.$t("about.panel"));
+        this.getLicenses();
     },
     methods: {
+        getLicenses: function () {
+            var self = this;
+            this.$http.get(window.baseurl + '/api/v1/licenses', function (data, status) {
+                self.isLoading = false;
+                self.$set("licenses", data);
+            });
+        },
         show: function (lic) {
             jQuery('#' + lic).openModal();
         }
