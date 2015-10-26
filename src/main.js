19722a4419c915cd754b942645b272a4f8bb0b20
Michał Frąckiewicz <michal@lvlup.pro>
2015-10-26 16:11:19 +0100

    v0.90.1 - bump, fix for OVH dedicated servers

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ require("font-awesome-webpack");
 require("../node_modules/materialize-css/dist/js/materialize.js")
 require("../assets/date.format.js")
 
-window.version = "0.90";
+window.version = "0.90.1";
 
 //customize right top dropdown
 setTimeout(function () {
