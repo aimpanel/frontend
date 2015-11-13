ef6122eba3cdfa2783ba2824cd3da2caad7e0666
Michał Frąckiewicz <michal@lvlup.pro>
2015-11-13 20:40:55 +0100

    v0.91

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ require("font-awesome-webpack");
 require("../node_modules/materialize-css/dist/js/materialize.js");
 require("../assets/date.format.js")
 
-window.version = "0.90.3";
+window.version = "0.91";
 
 //customize right top dropdown
 setTimeout(function () {
